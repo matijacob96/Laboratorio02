f71efe6a6859948118ae2c837c532cb069d2a3cb
matijacob96 <matijacob@live.com>
2018-09-27 21:52:08 -0300

    Resuelve Punto 2 Lab 03 Parte 2

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/EstadoPedidoReceiver.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/EstadoPedidoReceiver.java
@@ -1,12 +1,16 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
+import android.app.Notification;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.widget.Toast;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.PedidoRepository;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Pedido;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.PedidoDetalle;
 
 public class EstadoPedidoReceiver extends BroadcastReceiver {
 
@@ -14,10 +18,27 @@ public class EstadoPedidoReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         PedidoRepository repoaux;
         Pedido p;
+        Double varaux = 0.0;
 
         repoaux = new PedidoRepository();
 
         p = repoaux.buscarPorId(intent.getExtras().getInt("idPedido"));
-        Toast.makeText(context,"Pedido para "+p.getMailContacto()+" ha cambiado al estado "+p.getEstado(),Toast.LENGTH_LONG).show();
+        //Toast.makeText(context,"Pedido para "+p.getMailContacto()+" ha cambiado al estado "+p.getEstado(),Toast.LENGTH_LONG).show();
+
+        for(PedidoDetalle d : p.getDetalle()){
+            varaux += d.getCantidad()*d.getProducto().getPrecio();
+        }
+
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(context, "CANAL01")
+                .setSmallIcon(R.drawable.ic_restaurant_black)
+                .setContentTitle("Tu pedido fue aceptado")
+                .setStyle(new NotificationCompat.InboxStyle()
+                        .addLine("El costo será de: $"+varaux)
+                        .addLine("El horario de retiro/envio es: "+p.getFecha().getTime())); //TODO CAMBIAR EL HORARIO
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+
+// notificationId is a unique int for each notification that you must define
+        notificationManager.notify(p.getId(), notification.build());
     }
 }
--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/MainActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/MainActivity.java
@@ -1,6 +1,9 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,10 +17,30 @@ public class MainActivity extends AppCompatActivity {
 
     private final int REQUEST_NUEVOPEDIDO = 0;
 
+    private void createNotificationChannel() {
+        // Crear el canal de notificaciones pero solo para API 26 io superior
+        // dado que NotificationChannel es una clase nueva que no está incluida
+        // en las librerías de soporte qeu brindan compatibilidad hacía atrás
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Canal de prueba"; //TODO Cambiar esto para que no se hardcodee.
+            String description = "Descripcion de prueba";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("CANAL01", name, importance);
+            channel.setDescription(description);
+            // Registrar el canal en el sistema
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotificationChannel();
+
         btnNuevoPedido = (Button) findViewById(R.id.btnMainNuevoPedido);
         btnNuevoPedido.setOnClickListener(new View.OnClickListener() {
             @Override
