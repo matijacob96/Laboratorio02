e3b12ef1761410fe773e7c7bf130a06ab5506505
matijacob96 <matijacob@live.com>
2018-10-23 15:44:49 -0300

    Lab04-req03 faltan cosas por hacer, pero pausa de CS GO

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/CategoriaRest.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/CategoriaRest.java
@@ -3,8 +3,13 @@ package ar.edu.utn.frsf.dam.isi.laboratorio02;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
@@ -12,15 +17,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Categoria;
 
 public class CategoriaRest {
     // realiza el POST de una categoría al servidor REST
-    public void crearCategoria(Categoria c) throws  IllegalStateException {
+    public void crearCategoria(Categoria c) throws IllegalStateException {
 
         HttpURLConnection urlConnection = null;
         try {
@@ -30,7 +39,7 @@ public class CategoriaRest {
             InputStream in = null;
 //Crear el objeto json que representa una categoria
             JSONObject categoriaJson = new JSONObject();
-            categoriaJson.put("Nombre", c.getNombre());
+            categoriaJson.put("nombre", c.getNombre());
 //Abrir una conexión al servidor para enviar el POST
             URL url = new URL("http://10.0.2.2:5000/categorias/");
             urlConnection = (HttpURLConnection) url.openConnection();
@@ -84,6 +93,76 @@ public class CategoriaRest {
         finally {
             if (urlConnection != null) urlConnection.disconnect();
         }
+    }
+
+    public List<Categoria> listarTodas() throws IllegalStateException{
+// inicializar variables
+        HttpURLConnection urlConnection = null;
+        List<Categoria> resultado = null;
+        try {
+            resultado = new ArrayList<>();
+
+            InputStream in = null;
+// GESTIONAR LA CONEXION
+            URL url = new URL("http://10.0.2.2:5000/categorias/");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestProperty("Accept-Type", "application/json");
+            urlConnection.setRequestMethod("GET");
+// Leer la respuesta
+            in = new BufferedInputStream(urlConnection.getInputStream());
+            InputStreamReader isw = new InputStreamReader(in);
+            StringBuilder sb = new StringBuilder();
+            int data = isw.read();
+// verificar el codigo de respuesta
+            if (urlConnection.getResponseCode() == 200 ||
+                    urlConnection.getResponseCode() == 201) {
+                while (data != -1) {
+                    char current = (char) data;
+                    sb.append(current);
+                    data = isw.read();
+                }
+                // ver datos recibidos
+                Log.d("LAB_04", sb.toString());
+                // Transformar respuesta a JSON
+                JSONTokener tokener = new JSONTokener(sb.toString());
+                JSONArray listaCategorias = (JSONArray) tokener.nextValue();
+
+
+
+            /*Gson gson = new Gson();
+            Type type = new TypeToken<List<Categoria>>(){}.getType();
+            resultado = gson.fromJson(listaCategorias.toString(), type);*/
+
+
+                // iterar todas las entradas del arreglo
+                for (int i = 0; i < listaCategorias.length(); i++) {
+                    Categoria cat = new Categoria();
+                    // analizar cada element del JSONArray
+                    //armar una instancia de categoría y agregarla a la lista
+                    cat.setId(listaCategorias.optJSONObject(i).getInt("id"));
+                    cat.setNombre(listaCategorias.optJSONObject(i).getString("nombre"));
+                    resultado.add(cat);
+                }
+            } else {
+                // lanzar excepcion o mostrar mensaje de error
+                // que no se pudo ejecutar la operacion
+
+                throw new IllegalStateException():
+            }
+//NO OLVIDAR CERRAR inputStream y conexion
+            if (in != null) in.close();
+
+            if (urlConnection != null) urlConnection.disconnect();
+
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        } catch (JSONException e2) {
+            e2.printStackTrace();
+        }
+
+        //retornar resultado
+        return resultado;
     }
 
 }
