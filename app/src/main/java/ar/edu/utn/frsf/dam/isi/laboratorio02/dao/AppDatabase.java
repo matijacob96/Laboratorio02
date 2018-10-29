8d9fea2fa3a60b4098a91edb41cfa7e2ef6226db
Matías <matijacob@live.com>
2018-10-29 17:21:13 -0300

    Se terminó el requerimiento 6 del lab 4 parte 2

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/dao/AppDatabase.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/dao/AppDatabase.java
@@ -4,11 +4,13 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Categoria;
+import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Producto;
 
 
-@Database(entities = {Categoria.class}, version = 1)
+@Database(entities = {Categoria.class, Producto.class}, version = 1, exportSchema = false)
 
     public abstract class AppDatabase extends RoomDatabase {
         public abstract CategoriaDao categoriaDao();
+        public abstract ProductoDao productoDao();
     }
 
